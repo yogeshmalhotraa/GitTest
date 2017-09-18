676e22f6589cdde94b4aeb326718a475ba3c5300
Yogesh <yogeshmalhotraa>
2017-09-18 09:36:45 +0530

    First Change

--- GitTest/src/com/test/pkg/HelloWorld.java
+++ GitTest/src/com/test/pkg/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, Its first github project.");
+		System.out.println("Test1.");
 	}
 
 }
